d68ff09be2b521a6f938c238737772bfcf1c31a7
OKT <rudxor97@naver.com>
2021-09-25 21:17:12 +0900

    noto sans 적용

--- src/components/Layouts/index.tsx
+++ src/components/Layouts/index.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import emotionReset from "emotion-reset";
 import { Global, css } from "@emotion/react";
 
+const styles = css`
+  ${emotionReset};
+
+  body {
+    font-family: "Noto Sans Korean";
+  }
+`;
+
 function Layout(props) {
-  return <Global styles={emotionReset} />;
+  return <Global styles={styles} />;
 }
 
 export default Layout;
