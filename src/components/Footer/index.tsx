a84b9bc1b5eca24ddb54ec57df114190d4de2f5a
OKT <rudxor97@naver.com>
2021-10-10 18:59:05 +0900

    github icon 출력 및 copyright 출력

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,15 +4,16 @@ import React from "react";
 function Footer(props) {
   return (
     <FooterStyler>
-      <a href="https://github.com/OhGyeongtaek/blog">
-        https://github.com/OhGyeongtaek/blog
-      </a>
+      <LinkGithub href="https://github.com/OhGyeongtaek/blog">
+        <i className="fab fa-github"></i>
+      </LinkGithub>
+      <CopyRight>Copyright © BY GyeongLog 2021</CopyRight>
     </FooterStyler>
   );
 }
 
 const FooterStyler = styled.footer`
-  padding: 40px 0;
+  padding: 30px 0;
   text-align: center;
   border-top: 1px solid #ccc;
   background: #eee;
@@ -20,6 +21,20 @@ const FooterStyler = styled.footer`
   & a {
     color: #666;
   }
+`;
+
+const LinkGithub = styled.a`
+  font-size: 2rem;
+
+  &:hover {
+    color: #333;
+  }
+`;
+
+const CopyRight = styled.p`
+  padding-top: 8px;
+  font-size: 0.8rem;
+  color: #666;
 `;
 
 export default Footer;
