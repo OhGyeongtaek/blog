a9ed2cf4d4b7afad8b2df94626a9325dc0414cb1
OKT <rudxor97@naver.com>
2022-03-09 18:09:39 +0900

    Netlify를 이용하여 HTTPS 무료 호스팅하기 포스팅

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,7 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 800,
+              wrapperStyle: "margin:10px 0; background: red;",
             },
           },
         ],
